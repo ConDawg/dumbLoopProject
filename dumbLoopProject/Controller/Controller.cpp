ca9e35ee3e436b697ddac2e687b9791a46686089
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-01-25 13:46:19 -0700

    Created my number loop, my name var, and my statements.

---
+++ dumbLoopProject/Controller/Controller.cpp
@@ -0,0 +1,31 @@
+//
+//  Controller.cpp
+//  dumbLoopProject
+//
+//  Created by Johnson, Connor on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+using namespace std;
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    string name = "Connor Johnson";
+    cout << name << endl;
+    
+    for (int index = 13; index < 32; index ++)
+    {
+        cout << index  << " " ;
+        
+    }
+    
+    cout << endl << "Wuss poppin? " << "how are you?" << " whats good?" << endl;
+  
+}
---
+++ dumbLoopProject/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  dumbLoopProject
+//
+//  Created by Johnson, Connor on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+}
